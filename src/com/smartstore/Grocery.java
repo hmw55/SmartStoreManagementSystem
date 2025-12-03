a494eed26d4d13a1d1ba0be0f1351a3a2752f001
hmw55 <hollandwesley55@gmail.com>
2025-12-03 15:43:56 -0700

    Add option to choose preset or custom grocery items

--- src/com/smartstore/Grocery.java
+++ src/com/smartstore/Grocery.java
@@ -1,28 +1,19 @@
 package com.smartstore;
 
 public class Grocery {
-<<<<<<< HEAD
-=======
 
->>>>>>> feature/store-app
     private String name;
     private double price;
     private int quantity;
 
-<<<<<<< HEAD
-=======
     // Constructor
->>>>>>> feature/store-app
     public Grocery(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
 
-<<<<<<< HEAD
-=======
     // Getters
->>>>>>> feature/store-app
     public String getName() {
         return name;
     }
@@ -35,14 +26,11 @@ public class Grocery {
         return quantity;
     }
 
-<<<<<<< HEAD
-=======
     // Optional setters
     public void setName(String name) {
         this.name = name;
     }
 
->>>>>>> feature/store-app
     public void setPrice(double price) {
         this.price = price;
     }
